f3de8ab46f07eea210080fc51126cecde950cb9d
yumironov <mironoff2007@rambler.ru>
2019-03-26 14:18:44 +0300

    SQL create table corrected

--- src/main/resources/static/css/create_table.sql
+++ src/main/resources/static/css/create_table.sql
@@ -2,27 +2,26 @@ CREATE TABLE users (
 "username" varchar Not NULL ,
 "password" varchar Not NULL ,
 "enabled" varchar Not NULL DEFAULT 1 ,
-"userid" serial Not null,
-PRIMARY KEY ("userid")
+PRIMARY KEY ("username")
 );
 
 
 
 CREATE table user_roles (
 "user_role_id" serial Not NULL ,
-"userid" varchar(43) Not NULL,
+"username" varchar Not NULL,
 role varchar(43) Not NULL,
 PRIMARY KEY ("user_role_id"),
-UNIQUE KEV uni_username_role (role,username),
-KEV fk_username_dx (username),
-CONSTRAINT fk_username FOREIGN KEY (userid) REFERENCES users (userid));
+CONSTRAINT uni_username_role UNIQUE (role,username),
+
+CONSTRAINT fk_username FOREIGN KEY (username) REFERENCES users (username));
 
 
 
-INSERT INTO users(username,password,enahled)
+INSERT INTO users(username,password,enabled)
 VALUES ('priya','priya', true);
 
-INSERT INTO users(username,password,enahled)
+INSERT INTO users(username,password,enabled)
 VALUES  ('naveen','naveen', true);
 
 INSERT INTO user_roles (username, role)
