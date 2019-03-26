bf87d6bcce1b061ba96272f3385d7c757b67091c
yumironov <mironoff2007@rambler.ru>
2019-03-26 14:37:03 +0300

    JBDC setup corrected, jaxb dependency added

--- src/main/java/hello/MvcConfig.java
+++ src/main/java/hello/MvcConfig.java
@@ -22,10 +22,10 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
     @Bean(name = "dataSource")
 	public DriverManagerDataSource dataSource() {
 	    DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
-	    driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
-	    driverManagerDataSource.setUrl("jdbc:mysql://localhost:3306/userbase");
-	    driverManagerDataSource.setUsername("root");
-	    driverManagerDataSource.setPassword("root");
+	    driverManagerDataSource.setDriverClassName("org.postgresql.Driver");
+	    driverManagerDataSource.setUrl("jdbc:postgresql://localhost:5432/postgres");
+	    driverManagerDataSource.setUsername("admin");
+	    driverManagerDataSource.setPassword("admin");
 	    return driverManagerDataSource;
 	}
     
